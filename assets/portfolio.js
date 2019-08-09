6861e8a4df1ba5b8b6aa988d9562925f2f85cfb5
Derek Miller <derekmlmt@yahoo.com>
2019-08-08 18:22:51 -0700

    expanding on all the implementations, and beginning with javascript to Append info from the project's stored values.

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -5,18 +5,19 @@ $(document).ready(function() {
     console.log("document loaded");
 
 $(".project-wrap").on("mouseenter", function() {
-console.log("hovering on project: " + $(this).attr("data-title"));
-// $(".display").empty();
-var h2 = $("<h2>").addClass(".project-details");
-h2.text($(this).attr("data-title"));
-var p = $("<p>").addClass(".project-details");
-p.text($(this).attr("data-about"));
-$(".display").append(h2, p);
+    console.log("hovering on project: " + $(this).attr("data-title"));
+    // $(".display").empty();
+    var h2 = $("<h2>").addClass("project-details h2");
+    h2.text($(this).attr("data-title"));
+    var p = $("<p>").addClass("project-details p");
+    p.text($(this).attr("data-about"));
+    $(".display").append(h2, p);
 
 })
 
 $(".project-wrap").on("mouseleave", function() {
-$(".display").empty();
+    console.log("mouse left: " + $(this).attr("data-title"));
+    $(".display").empty();
 })
 
 

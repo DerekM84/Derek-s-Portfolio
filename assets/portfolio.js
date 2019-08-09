d07b0b3856e62798c674dba8fa29dfb6e8449aba
Derek Miller <derekmlmt@yahoo.com>
2019-08-08 17:24:25 -0700

    Connected project links, and beginning to add information about them. Working on getting it displayed to the main display with javascript mouseover/leaves.

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -4,6 +4,21 @@ $(document).ready(function() {
     
     console.log("document loaded");
 
+$(".project-wrap").on("mouseenter", function() {
+console.log("hovering on project: " + $(this).attr("data-title"));
+// $(".display").empty();
+var h2 = $("<h2>").addClass(".project-details");
+h2.text($(this).attr("data-title"));
+var p = $("<p>").addClass(".project-details");
+p.text($(this).attr("data-about"));
+$(".display").append(h2, p);
+
+})
+
+$(".project-wrap").on("mouseleave", function() {
+$(".display").empty();
+})
+
 
 
 })
